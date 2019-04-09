0623e12a14cc7f79fd89b844efdf1230672c81c1
Heather Kirk <hskirk@gmail.com>
2019-04-09 00:58:24 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,6 +1,7 @@
 class Students < ActiveRecord::Migration[5.1]
   
   def change
-    add_column :students, :grade
+    add_column :students, :grade, :integer 
+    add_column :students, :birthdate, :string 
   end 
 end 
