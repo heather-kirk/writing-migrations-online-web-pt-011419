d12e6f7fbfa7971f52bce532cd1e1c1bb1ba2a01
Heather Kirk <hskirk@gmail.com>
2019-04-09 01:04:16 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,4 +1,4 @@
-class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
+class ChangeDatatypeForBirthday < ActiveRecord::Migration[5.1]
   
   def change
     change_column :students, :birthdate,:datetime
